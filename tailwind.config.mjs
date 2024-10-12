8f3e74f72ac374025ff684d7ad3faa784471f376
Sophie <soph1155@stud.kea.dk>
2024-10-12 13:39:35 +0200

    tilføj flip card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				'infinite-scroll': 'infinite-scroll 25s linear infinite',
-			},
-			keyframes: {
-				'infinite-scroll': {
-					from: { transform: 'translateX(0)' },
-					to: { transform: 'translateX(-100%)' },
-				},
-			},
-		},
-	},
-	plugins: [
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-100%)' },
+                },
+            },
+            rotate: {
+                'y-180': '180deg', // Tilføj roteringsklasse
+            },
+        },
+    },
+    plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
-}
+};
